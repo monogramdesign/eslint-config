d3a90a44a8c51b4850302c3b1901a57333d65e22
Leonardo Ortiz <leo@monogram.io>
2022-02-21 13:24:41 -0300

    feat: add commitlint closes #2

---
+++ commitlint.config.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-import-module-exports
+import type { UserConfig } from '@commitlint/types'
+
+const Configuration: UserConfig = {
+	/*
+	 * Resolve and load @commitlint/config-conventional from node_modules.
+	 * Referenced packages must be installed
+	 */
+	extends: ['@commitlint/config-conventional']
+}
+
+module.exports = Configuration
