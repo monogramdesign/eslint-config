3b23dc5c43dcde2d698428b37d5c7722b401f160
Leonardo Ortiz <leo@monogram.io>
2022-03-24 18:39:08 -0300

    nextjs-ts config, closes #4

--- src/configs.ts
+++ src/configs.ts
@@ -1,3 +1,4 @@
+// TODO: move each config to its own file then import here
 export const configs = {
 	nextjs: {
 		config: {
@@ -10,27 +11,39 @@ export const configs = {
 			],
 			rules: {
 				indent: 'off',
-				'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+				'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
 				'jsx-a11y/anchor-is-valid': 'off' // TODO: enable this rule for anchors that are not within Next.js Link component
 			}
 		},
 		dependencies: ['eslint-config-prettier', 'eslint-config-airbnb', 'eslint-config-next']
 	},
-	typescript: {
+	'nextjs-ts': {
 		config: {
 			parser: '@typescript-eslint/parser',
-			plugins: ['@typescript-eslint'],
+			plugins: ['@typescript-eslint', 'import'],
+			settings: {
+				'import/parsers': {
+					'@typescript-eslint/parser': ['.ts', '.tsx']
+				},
+				'import/resolver': {
+					typescript: {
+						alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+					}
+				}
+			},
+
 			extends: [
 				'airbnb',
 				'airbnb/hooks',
 				'eslint:recommended',
 				'plugin:@typescript-eslint/recommended',
+				'next', // = eslint-plugin-react / eslint-plugin-react-hooks / eslint-plugin-next
+				'next/core-web-vitals',
 				'prettier'
 			],
 			rules: {
 				indent: 'off',
 				'no-use-before-define': 'off',
-				'no-console': 'off',
 				'no-else-return': [
 					'error',
 					{
@@ -38,20 +51,16 @@ export const configs = {
 					}
 				],
 				'import/extensions': 'off',
-				'import/prefer-default-export': 'off',
-				'@typescript-eslint/ban-ts-comment': 'off'
-			},
-			settings: {
-				'import/resolver': {
-					node: {
-						extensions: ['.ts']
-					}
-				}
+				'@typescript-eslint/ban-ts-comment': 'off',
+				'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+				'react/jsx-props-no-spreading': 'off'
 			}
 		},
 		dependencies: [
 			'eslint-config-prettier',
 			'eslint-config-airbnb',
+			'eslint-plugin-import',
+			'eslint-import-resolver-typescript',
 			'@typescript-eslint/eslint-plugin',
 			'@typescript-eslint/parser'
 		]
