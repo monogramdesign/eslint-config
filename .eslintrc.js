4dba084120546c80f31e2f43d14ccc66cab1ea59
Leonardo Ortiz <leo@monogram.io>
2022-04-07 20:39:18 -0300

    use node config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint",
+    "import"
+  ],
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [
+        ".ts"
+      ]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true
+      }
+    }
+  },
+  "extends": [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  "rules": {
+    "indent": "off",
+    "no-use-before-define": "off",
+    "no-else-return": [
+      "error",
+      {
+        "allowElseIf": true
+      }
+    ],
+    "import/extensions": "off"
+  }
+}
