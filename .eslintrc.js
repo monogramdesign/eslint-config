fe9bb0b73ec1b956f2e1e20b3511de509bff16f5
Leonardo Ortiz <leo@monogram.io>
2022-07-20 21:17:35 -0300

    chore: use node config internally

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,31 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('./dist/configs/node').default
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'import'],
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts']
+		},
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true
+			}
+		}
+	},
+	extends: [
+		'airbnb-base',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	rules: {
+		indent: 'off',
+		'no-use-before-define': 'off',
+		'no-else-return': [
+			'error',
+			{
+				allowElseIf: true
+			}
+		],
+		'import/extensions': 'off'
+	}
+}
