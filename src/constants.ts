3a25ddcd7de92fb27b31646a659486f6e23adc04
Leonardo Ortiz <leo@monogram.io>
2025-02-20 20:14:12 -0300

    feat: support bun

--- src/constants.ts
+++ src/constants.ts
@@ -6,16 +6,18 @@ export const ESLINT_FILENAME = '.eslintrc.js'
 
 export const AVAILABLE_CONFIGS = { next: 'Next.js', node: 'Node.js' } as const
 
-export const PACKAGE_MANAGERS = ['yarn', 'pnpm', 'npm'] as const
+export const PACKAGE_MANAGERS = ['npm', 'yarn', 'pnpm', 'bun'] as const
 
 export const LOCK_FILES: PackageManagerRecord = {
-	yarn: `${process.cwd()}/yarn.lock`,
-	pnpm: `${process.cwd()}/pnpm-lock.yaml`,
-	npm: `${process.cwd()}/package-lock.json`
+	npm: 'package-lock.json',
+	yarn: 'yarn.lock',
+	pnpm: 'pnpm-lock.yaml',
+	bun: 'bun.lockb'
 } as const
 
 export const INSTALL_PREFIXES: PackageManagerRecord = {
 	yarn: 'yarn add -D',
 	pnpm: 'pnpm add -D',
-	npm: 'npm i -D'
+	npm: 'npm i -D',
+	bun: 'bun add -D'
 } as const
